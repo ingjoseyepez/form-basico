8079f7f31d0a6010db2624f245e82617aa4e2c2c
jose Yepez <ing.joseyepez@gmail.com>
2023-06-10 18:40:52 -0500

    [add] creacion de formulario

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulario</title>
+</head>
+<body>
+	<form action="index.php" method="POST">
+	<?php
+			
+			if(isset($_POST['nombre'])){
+				$nombre = $_POST['nombre'];
+				$password = $_POST['password'];
+				$email = $_POST['email'];
+
+				$campos = array();
+
+				if($nombre == ""){
+					array_push($campos, "El campo Nombre no pude estar vacío");
+				}
+
+				if($password == "" || strlen($password) < 6){
+					array_push($campos, "El campo Password no puede estar vacío, ni tener menos de 6 caracteres.");
+				}
+
+				if($email == "" || strpos($email, "@") === false){
+					array_push($campos, "Ingresa un correo electrónico válido.");
+				}
+
+				if(count($campos) > 0){
+					echo "<div class='error'>";
+					for($i = 0; $i < count($campos); $i++){
+						echo "<li>".$campos[$i]."</i>";
+					}
+				}else{
+					echo "<div class='correcto'>
+							Datos correctos";
+				}
+				echo "</div>";
+			}
+		?>
+		<p>
+		Nombre:<br/>
+		<input type="text" name="nombre">
+		</p>
+
+		<p>
+		Password:<br/>
+		<input type="password" name="password">
+		</p>
+
+		<p>
+		correo electrónico:<br/>
+		<input type="text" name="email">
+		</p>
+
+		<p><input type="submit" value="enviar datos"></p> 
+	</form>
+</body>
+</html>
